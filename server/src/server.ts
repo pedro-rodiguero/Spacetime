df57ee4d9fc2b2a4845046ba71bb8f142ccc7ba2
pemiinem <190139323@aluno.unb.br>
2023-05-19 21:29:24 -0300

    add github auth on the backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,18 @@
+import 'dotenv/config'
+
 import fastify from 'fastify'
+import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
 
+app.register(cors, {
+  origin: true,
+})
+
 app.register(memoriesRoutes)
+app.register(authRoutes)
 
 app
   .listen({
